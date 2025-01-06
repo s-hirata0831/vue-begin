8ba9c5cc85b53d747804dab3c602bf715a6f184e
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2025-01-06 14:22:26 +0900

    feat: 入力反映とボックス削除を実装

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,9 @@ export default{
     data(){
         return{
             message: "こんにちは！Vue3へようこそ！",
-            changeLang: "Hello, Vue!"
+            changeLang: "Hello, Vue!",
+            input: '',
+            show: true
         };
     },
     methods: {
@@ -18,9 +20,44 @@ export default{
 
 <template>
     <h1>Home</h1>
-    {{ message }}
+    <p>{{ message }}</p>
     <h2>クリックしたら何か起こる系(イベントハンドリング)</h2>
-    {{ changeLang }}
-    <br>
+    <p>{{ changeLang }}</p>
     <button @click="japanese">日本語表示</button>
+    <h2>入力したら何か起こる系</h2>
+    <p>Hello {{ input }}!</p>
+    <input v-model="input" placeholder="何かを入力" type="text">
+    <h2>ボックスを消す</h2>
+    <div id="box" v-if="show" class="panel">
+        <p>エラーメッセージ表示時に便利かもしれません</p>
+        <button @click="show =! show">
+            閉じる
+        </button>
+    </div>
 </template>
+
+<style>
+.panel {
+  background: #fee;
+  border: 2px solid #fbb;
+  padding: 20px;
+  text-align: center;
+}
+button {
+  font-size: 1rem;
+  background: none;
+  color: #f66;
+  border-radius: 10px;
+  border-color: #d44;
+  cursor: pointer;
+  outline: none;
+}
+button span {
+  font-size: 2.25rem;
+  position: relative;
+  top: 5px;
+}
+button:hover {
+  color: #d44;
+}
+</style>
