bbe5819f53dac213e67ef4008f47f33e33fa2158
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2025-01-06 17:03:19 +0900

    fix: ビルドの設定変更

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path' // path モジュールをインポート
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/vue-begin/' : '/',
+  base: './',
   build: {
     outDir: 'docs',
     rollupOptions: {
