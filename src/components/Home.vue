c87dde743ec4e5b3f6c2a7e279324240d18bbfac
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2025-01-06 13:50:37 +0900

    feat: イベントハンドリングを実装

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,26 @@
+<script>
+import { ref } from 'vue'
+
+export default{
+    data(){
+        return{
+            message: "こんにちは！Vue3へようこそ！",
+            changeLang: "Hello, Vue!"
+        };
+    },
+    methods: {
+        japanese: function(){
+            this.changeLang = 'こんにちは！びゅーーー！'
+        }
+    }
+};
+</script>
+
 <template>
     <h1>Home</h1>
+    {{ message }}
+    <h2>クリックしたら何か起こる系(イベントハンドリング)</h2>
+    {{ changeLang }}
+    <br>
+    <button @click="japanese">日本語表示</button>
 </template>
